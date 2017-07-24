5dc6b8e27ceb7c973ad8eb960bab316235dd8396
Aaron Nguyen <aaron3831@gmail.com>
2017-07-24 00:39:10 -0700

    add  ajax for show products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,15 @@ class ProductsController < ApplicationController
     @products = Product.all
     @order_item = current_order.order_items.new
   end
+  def show
+    @product = Product.find(params[:id])
+    @order_item = current_order.order_items.new
 
+    respond_to do |format|
+      format.html {redirect_to products_path}
+      format.js
+    end
+  end
   def new
     @product = Product.new
   end
